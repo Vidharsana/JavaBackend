01ec05e596245eea8e1a2ef110449a19ff96de24
Vidharsana <vidharsanasundharraj@gmail.com>
2025-03-10 15:06:17 +0530

    Session and Filters

---
+++ SessionNFilters/src/main/java/com/test/Login.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String name = request.getParameter("user");
+		Cookie ck = new Cookie("info",name);
+		response.addCookie(ck);
+		out.println("from login page : "+name);
+		out.println("<a href = 'Logout'> logout here</a>");
+		
+	}
+
+}
