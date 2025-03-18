91a3fa7a304908dce4557a29c638a4facfcd11d4
Vidharsana <vidharsanasundharraj@gmail.com>
2025-03-18 17:19:28 +0530

    SpringBoot MicroServices

---
+++ SpringBootMicroService1/src/main/java/com/test/SpringBootMicroService1Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroService1Application.class, args);
+	}
+
+}
